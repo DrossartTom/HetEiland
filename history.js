7412753c1bebff85e8f26981d2e232ce7fa951c2
Tom Drossart <tom.drossart@gmail.com>
2024-05-11 18:48:40 +0200

    test

---
+++ history.js
@@ -0,0 +1,25 @@
+
+/* variabelen */
+const aiButtons = document.querySelectorAll(".ai-button");
+const aiInfos = document.querySelectorAll(".ai-info");
+const closeButton = document.querySelectorAll(".close-button");
+
+/* event */
+aiButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const target = button.getAttribute("data-target");
+        aiInfos.forEach(info => {
+            if (info.id === `${target}-info`) {
+                info.classList.add("active");
+            } else {
+                info.classList.remove("active");
+            }
+        });
+    });
+});
+
+closeButton.forEach(button => {
+    button.addEventListener("click", () => {
+        button.parentElement.classList.remove("active");
+    });
+});
